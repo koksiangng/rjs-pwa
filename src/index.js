fcb770834e039fc98e378860b99dd3a1f95ebfce
unknown <ksng@kth.se>
2022-06-27 16:31:02 +0200

    master -> main

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<h1>Hello, World!</h1>)
+//const root = ReactDOM.createRoot(document.getElementById('root')); //Root in "index.html"
+//root.render(<h1>Hello, World!</h1>)
+
+ReactDOM.render(<App />, document.getElementById('root'));
