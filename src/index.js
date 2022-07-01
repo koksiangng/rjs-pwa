8a1d8abec2ae3efc2453174fcb5e9238bd400c97
unknown <ksng@kth.se>
2022-07-01 17:54:41 +0200

    reactjs stuff

--- src/index.js
+++ src/index.js
@@ -2,11 +2,7 @@ import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 
-
-//const root = ReactDOM.createRoot(document.getElementById('root'));
-//root.render(<h1>Hello, World!</h1>)
-
-//Root in "index.html"
+//StrictMode -> Gives warnings.
 const root = createRoot(document.getElementById('root'));
 root.render(
     <StrictMode>
